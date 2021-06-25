a3d7045d8433ac08bb7fc1e7885a7cc4f5b762b9
philippe-bourdeau <bourdeau.phil@gmail.com>
2021-06-25 17:37:03 -0400

    fix test

--- tests/compound.test.js
+++ tests/compound.test.js
@@ -1,5 +1,6 @@
 const utils = require('../src/utils');
+const lodash = require ('lodash')
 
 test('compound 1 year', () => {
-    expect(utils.compounds(2.52,1, 5)).toBe(2.65);
+    expect(lodash.round(utils.compounds(2.52,1, 5),2 )).toBe(2.65);
 });
